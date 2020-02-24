45a72b28f2198bc4440467cf49dd951b35443778
jassantos <jacessantos@gmail.com>
2020-02-24 17:21:30 +0800

    Updates

---
+++ precache-manifest.37c33e10dcfa7de67e32c4f08970928c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4475680cb5bf90c6328260c93d3cc9a7",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "2d1ed2280f154426720d",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2564bf62c63fd5749fb6",
+    "url": "/my-app/static/js/2.5aea92c6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.5aea92c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1ed2280f154426720d",
+    "url": "/my-app/static/js/main.52a6bdb0.chunk.js"
+  },
+  {
+    "revision": "23cadb8593a900bae0c7",
+    "url": "/my-app/static/js/runtime-main.913f3e83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
